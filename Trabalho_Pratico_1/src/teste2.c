153ec74930dd6b1a9a8708ee3cb589ebe2af122b
Vinicius <viniciushallmann2015@gmail.com>
2023-02-26 17:37:36 -0300

    commits testes Dr.Memory

---
+++ Trabalho_Pratico_1/src/teste2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int JeitosDeSubir( int n );
+int main(){
+    int n = 5;
+    printf( "%d", JeitosDeSubir( n ) );
+}
+
+int JeitosDeSubir( int n ){
+    int passoAtual = 0, passoAnterior = 1, totalPassos, i;
+
+    if ( n <= 3 )
+        return n;
+    
+    for( i = n; i > 0; i-- ){
+        totalPassos = passoAtual + passoAnterior;
+        passoAtual = passoAnterior;
+        passoAnterior = totalPassos;
+    }
+    return totalPassos;
+}
