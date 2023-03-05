f709efb349808e5459a0124c268ce0cdc296a6d9
Vinicius <viniciushallmann2015@gmail.com>
2023-03-05 12:55:18 -0300

    commit correção de código

--- Trabalho_Pratico_1_real/src/main.c
+++ Trabalho_Pratico_1_real/src/main.c
@@ -8,38 +8,42 @@ int judgeCircle( char *moves );
 
 int main( int argc, char const *argv[] )
 {
-    char *moves;
+    //Mudança na inicialização do moves. No código feito em aula eu acabei esquecendo de inicializar o char *moves.
+    char *moves = malloc(sizeof (moves));
 
     printf( "Entrada de movimentos: " );
-    scanf( "%s", &moves );
+    scanf( "%s", moves );
     judgeCircle ( moves );
     return 0;
     
 }
 
-int judgeCircle( char *moves ){
+int judgeCircle( char * moves ){
 
     int i, contadorR = 0, contadorL = 0, contadorD = 0, contadorU = 0;
 
     for ( i = 0; i < strlen( moves ); i++ ){
-        if ( moves[i] == 'R' )
+        if ( moves[i] == 'R' ){
             contadorR += 1;
-        if ( moves[i] == 'L' )
+        }
+        if ( moves[i] == 'L' ){
             contadorL += 1;
-        if ( moves[i] == 'U' )
+        }
+        if ( moves[i] == 'U' ){
             contadorU += 1;
-        if ( moves[i] == 'D' )
+        }    
+        if ( moves[i] == 'D' ){
             contadorD += 1;
-    }   
-
+        } 
+    }  
+    
+    // Mudança feita no if para comparar se os contadores de todas as direções estão iguais.
     if (( contadorR == contadorL) &&  (contadorD == contadorU )){
         printf( "Verdadeiro" );
         return 1;
     } else {
         printf( "Falso" );
-            return 0;
+        return 0;
     }
-        
-        
 }
 
