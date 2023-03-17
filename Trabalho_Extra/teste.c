f47c05ba55d201c8051a9ea78b9ad4cfb83fe52f
Vinícius <viniciushallmann2015@gmail.com>
2023-03-17 16:50:35 -0300

    Delete Trabalho_Extra directory

--- Trabalho_Extra/teste.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main(){
-    char *str1 = {"a"}, *str2 = {"b"};
-    printf("%d\n", strcmp(str1, str2)); // -1
-    printf("%d", strcmp(str2, str1)); // 1
-}
