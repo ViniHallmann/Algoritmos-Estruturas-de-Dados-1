f4c0d324a364633ee17ae524d8caae36b086b349
Vinicius <viniciushallmann2015@gmail.com>
2023-04-27 14:58:24 -0300

    att para medir tempo de código

--- Prática_Algoritmos_de_Ordenação/Implementação_Selection_Sort/main.c
+++ Prática_Algoritmos_de_Ordenação/Implementação_Selection_Sort/main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define TAM 10
+#include <windows.h>
+
+#define TAM 1000
 
 int main(){
     int vetorDesordenado [TAM], menor, aux;
 
     srand(time(NULL));
     for ( int i = 0; i < TAM; i++){
-        vetorDesordenado [i] = rand () % 100;
+        vetorDesordenado [i] = rand () % 1000;
     }
     for ( int i = 0; i < TAM; i++ ){
         printf( "%d ", vetorDesordenado[i] );
     }
     printf("\n");
+
+    clock_t inicio, fim;
+    double tempoExecucao;
+    inicio = clock();
+
     for ( int i = 0; i < TAM; i++ ){
         menor = i;
         for ( int j = i; j < TAM; j++ ){
@@ -25,7 +32,16 @@ int main(){
         vetorDesordenado[menor] = vetorDesordenado[i];
         vetorDesordenado[i] = aux;
     }
+
+    fim = clock();
+    tempoExecucao = ( (double ) ( fim - inicio ) ) / CLOCKS_PER_SEC;
     for ( int i = 0; i < TAM; i++ ){
         printf( "%d ", vetorDesordenado[i] );
     }
+    printf( "\nTempo de execucao: %lf segundos\n", tempoExecucao );
+    //COPIEI ESSE CÓDIGO PARA LIMPAR O TERMINAL
+    clock_t start_time = clock();
+    int desired_time = 1; 
+    while ((clock() - start_time) / CLOCKS_PER_SEC < desired_time) {}
+    system( "cls" );
 }
